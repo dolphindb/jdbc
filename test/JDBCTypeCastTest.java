b1c55265385090f087fb5733baa634f46e048dc5
jhy <haoyang.jiang@dolphindb.com>
2022-05-13 10:44:36 +0800

    AJ-48:add testing case about other datatype

--- test/JDBCTypeCastTest.java
+++ test/JDBCTypeCastTest.java
@@ -1,13 +1,14 @@
-import com.xxdb.data.BasicMonth;
-import com.xxdb.data.BasicNanoTime;
-import com.xxdb.data.BasicNanoTimestamp;
+import com.xxdb.data.*;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import com.xxdb.data.BasicTimestamp;
 
 import java.sql.*;
+import java.time.LocalDateTime;
 import java.time.Month;
+import java.util.UUID;
 
 public class JDBCTypeCastTest {
     Connection conn;
@@ -39,8 +40,17 @@ public class JDBCTypeCastTest {
                     "col_int = 1 2 3 4\n" +
                     "col_nanotime =  09:08:01.001234567 09:08:01.001765432 09:08:01.001987654 09:08:01.981987654\n" +
                     "col_nanotimestamp =  2017.12.01T09:08:01.001234567 2017.12.01T09:08:01.001765432 2017.12.01T09:08:01.001987654 2018.12.01T09:08:01.671987654\n" +
-                    "tb = table(col_sym,col_date,col_time,col_month,col_dt,col_str,col_char,col_float,col_doub,col_int,col_nanotime,col_nanotimestamp)";
-
+                    "col_bool = true true false false\n" +
+                    "col_short = short(1 2 3 4)\n" +
+                    "col_long = long(1 2 3 4)\n" +
+                    "col_minute = 13:30m 13:30m 13:30m 13:30m\n" +
+                    "col_second = 13:30:10 13:30:10 13:30:10 13:30:10\n"+
+                    "col_datetime =  2017.12.01T09:08:01 2017.12.01T09:08:01 2017.12.01T09:08:01 2018.12.01T09:08:01\n" +
+                    "col_timestamp =  2012.06.13T13:30:10.008 2012.06.13T13:30:10.008 2012.06.13T13:30:10.008 2012.06.13T13:30:10.008\n" +
+                    "col_uuid =  rand(uuid(),4)\n" +
+                    "col_ipaddr =  rand(ipaddr(),4)\n" +
+                    "col_int128 =  rand(int128(),4)\n" +
+                    "tb = table(col_sym,col_date,col_time,col_month,col_dt,col_str,col_char,col_float,col_doub,col_int,col_nanotime,col_nanotimestamp,col_bool,col_short,col_long,col_minute,col_second,col_datetime,col_timestamp,col_uuid,col_ipaddr,col_int128)";
             System.out.println(sqlDDL);
             stm.executeUpdate(sqlDDL);
             //读取内存表到RecordSet
@@ -60,6 +70,16 @@ public class JDBCTypeCastTest {
                     int i = rs.getInt(10);
                     BasicNanoTime  nt = (BasicNanoTime)rs.getObject(11);
                     BasicNanoTimestamp ntp = (BasicNanoTimestamp) rs.getObject(12);
+                    Boolean bool = rs.getBoolean(13);
+                    short shorts = rs.getShort(14);
+                    long longs = rs.getLong(15);
+                    BasicMinute min = (BasicMinute)rs.getObject(16);
+                    BasicSecond sec = (BasicSecond) rs.getObject(17);
+                    BasicDateTime dt = (BasicDateTime) rs.getObject(18);
+                    BasicTimestamp times = (BasicTimestamp) rs.getObject(19);
+                    BasicUuid uuids = (BasicUuid) rs.getObject(20);
+                    BasicIPAddr ipaddrs = (BasicIPAddr)rs.getObject(21);
+                    BasicInt128 int128 = (BasicInt128)rs.getObject(22);
                 }catch(Exception ex){
                     Assert.fail(ex.getMessage());
                 }
@@ -69,13 +89,11 @@ public class JDBCTypeCastTest {
         {
             Assert.fail(ex.getMessage());
         }
-
-
-
     }
 
     @After
     public void Destroy(){
 
     }
+
 }
