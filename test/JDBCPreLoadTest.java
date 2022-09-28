88a9544e43051e5a853c9940bec9c268a158123c
wanglu121 <lu.wang@dolphindb.com>
2022-09-28 14:32:36 +0800

    DPLG-470：Add testcase of load table named by keyword

--- test/JDBCPreLoadTest.java
+++ test/JDBCPreLoadTest.java
@@ -20,19 +20,22 @@ public class JDBCPreLoadTest {
         DBConnection db = null;
 
         try{
-            String script = "n=500800\n" +
+            String script = "\n" +
+                    "n=500800;\n" +
                     "date=take(2006.01.01..2006.01.31, n);\n" +
                     "x=rand(10.0, n);\n" +
                     "t=table(date, x);\n" +
                     "id = rand(20.0,n);\n" +
                     "t2 = table(date,id);\n" +
-                    "login(\"admin\",\"123456\")\n" +
+                    "t3 = table(date,id,x)\n" +
                     "if(existsDatabase(\"dfs://valuedb\")){dropDatabase(\"dfs://valuedb\")}\n" +
                     "db=database(\"dfs://valuedb\", VALUE, 2006.01.01..2006.01.31)\n" +
                     "pt = db.createPartitionedTable(t, `pt, `date);\n" +
-                    "dt = db.createPartitionedTable(t2,`dt,`date)\n" +
+                    "dt = db.createPartitionedTable(t2,`dt, `date);\n" +
+                    "Order_tmp = db.createPartitionedTable(t3,`Order_tmp,`date);\n" +
                     "pt.append!(t);\n" +
-                    "dt.append!(t2);";
+                    "dt.append!(t2);\n" +
+                    "Order_tmp.append!(t3);";
             String script2 = "n = 50080000\n" +
                     "date = take(2006.01.01..2006.01.31,n);\n" +
                     "de = rand(30.0,n)\n" +
@@ -44,7 +47,54 @@ public class JDBCPreLoadTest {
                     "nt = db2.createPartitionedTable(t3,`nt,`date);\n" +
                     "nt.append!(t3);";
             db = new DBConnection();
-            db.connect(HOST, PORT);
+            db.connect(HOST, PORT,"admin","123456");
+            db.run(script);
+            db.run(script2);
+            success = true;
+        }catch(Exception e){
+            e.printStackTrace();
+            success = false;
+        }finally{
+            if(db != null){
+                db.close();
+            }
+            return success;
+        }
+    }
+
+    public static boolean createPartitionTable(String name){
+        boolean success = false;
+        DBConnection db = null;
+
+        try{
+            String script = "\n" +
+                    "n=500800;\n" +
+                    "date=take(2006.01.01..2006.01.31, n);\n" +
+                    "x=rand(10.0, n);\n" +
+                    "t=table(date, x);\n" +
+                    "id = rand(20.0,n);\n" +
+                    "t2 = table(date,id);\n" +
+                    "t3 = table(date,id,x)\n" +
+                    "if(existsDatabase(\"dfs://valuedb\")){dropDatabase(\"dfs://valuedb\")}\n" +
+                    "db=database(\"dfs://valuedb\", VALUE, 2006.01.01..2006.01.31)\n" +
+                    "pt = db.createPartitionedTable(t, `pt, `date);\n" +
+                    "dt = db.createPartitionedTable(t2,`dt, `date);\n" +
+                    name+" = db.createPartitionedTable(t3,`"+name+",`date);\n" +
+                    "pt.append!(t);\n" +
+                    "dt.append!(t2);\n" +
+                    name+".append!(t3);";
+            String script2 = "n = 50080000\n" +
+                    "date = take(2006.01.01..2006.01.31,n);\n" +
+                    "de = rand(30.0,n)\n" +
+                    "t3 = table(date,de)\n" +
+                    "if(existsDatabase(\"dfs://testValue\")){\n" +
+                    "    dropDatabase(\"dfs://testValue\")\n" +
+                    "}\n" +
+                    "db2 = database(\"dfs://testValue\",VALUE,2006.01.01..2006.01.31)\n" +
+                    "nt = db2.createPartitionedTable(t3,`nt,`date);\n" +
+                    "nt.append!(t3);";
+            db = new DBConnection();
+            db.connect(HOST, PORT,"admin","123456");
             db.run(script);
             db.run(script2);
             success = true;
@@ -149,4 +199,77 @@ public class JDBCPreLoadTest {
         }
         Assert.assertEquals(100,count);
     }
+
+    @Test
+    public void test_Order_tmp() throws SQLException {
+        conn = DriverManager.getConnection(url+"?databasePath=dfs://valuedb",info);
+        Statement stm = conn.createStatement();
+        ResultSet rs = stm.executeQuery("select TOP 101 * from Order_tmp");
+        Assert.assertTrue(rs.next());
+        int index = 0;
+        while(rs.next()){
+            System.out.println((index++)+":"+rs.getString(1)+" "+rs.getString(2));
+        }
+        Assert.assertEquals(100,index);
+    }
+
+    @Test
+    public void test_Group_tmp() throws SQLException {
+        Assert.assertTrue(createPartitionTable("Group_tmp"));
+        conn = DriverManager.getConnection(url+"?databasePath=dfs://valuedb",info);
+        Statement stm = conn.createStatement();
+        ResultSet rs = stm.executeQuery("select TOP 101 * from Group_tmp");
+        Assert.assertTrue(rs.next());
+        int index = 0;
+        while(rs.next()){
+            System.out.println((index++)+":"+rs.getString(1)+" "+rs.getString(2));
+        }
+        Assert.assertEquals(100,index);
+    }
+
+    @Test
+    public void test_Where_tmp() throws SQLException {
+        Assert.assertTrue(createPartitionTable("Where_tmp"));
+        conn = DriverManager.getConnection(url+"?databasePath=dfs://valuedb",info);
+        Statement stm = conn.createStatement();
+        ResultSet rs = stm.executeQuery("select TOP 101 * from Where_tmp");
+        Assert.assertTrue(rs.next());
+        int index = 0;
+        while(rs.next()){
+            System.out.println((index++)+":"+rs.getString(1)+" "+rs.getString(2));
+        }
+        Assert.assertEquals(100,index);
+    }
+
+    @Test
+    public void test_Where_Order_group() throws SQLException {
+        Assert.assertTrue(createPartitionTable("Where_Order_group"));
+        conn = DriverManager.getConnection(url+"?databasePath=dfs://valuedb",info);
+        Statement stm = conn.createStatement();
+        ResultSet rs = stm.executeQuery("select TOP 101 * from Where_Order_group");
+        Assert.assertTrue(rs.next());
+        int index = 0;
+        while(rs.next()){
+            System.out.println((index++)+":"+rs.getString(1)+" "+rs.getString(2));
+        }
+        Assert.assertEquals(100,index);
+    }
+
+    @Test(expected = AssertionError.class)
+    public void test_by() throws SQLException {
+        Assert.assertTrue(createPartitionTable("by"));
+        conn = DriverManager.getConnection(url+"?databasePath=dfs://valuedb",info);
+        Statement stm = conn.createStatement();
+        ResultSet rs = stm.executeQuery("select TOP 101 * from by");
+        Assert.assertTrue(rs.next());
+        int index = 0;
+        while(rs.next()){
+            System.out.println((index++)+":"+rs.getString(1)+" "+rs.getString(2));
+        }
+        Assert.assertEquals(100,index);
+    }
+
+
+
+
 }
