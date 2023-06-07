48038f71fb90558dd3618b2ca15b4c5b76b7af6a
chengyitian <sylvestercheng@aliyun.com>
2023-06-07 14:47:55 +0800

    JDBC-9: fix some type cast issues for matlab access jdbc to ddb.
    Former-commit-id: f3fcb60ef00caa0e0efb91b9288c2d196080093e

--- src/com/dolphindb/jdbc/JDBCResultSetMetaData.java
+++ src/com/dolphindb/jdbc/JDBCResultSetMetaData.java
@@ -1,9 +1,10 @@
 package com.dolphindb.jdbc;
 
 import com.xxdb.data.BasicTable;
-
+import com.xxdb.data.Entity;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Types;
 
 public class JDBCResultSetMetaData implements ResultSetMetaData{
 
@@ -46,7 +47,24 @@ public class JDBCResultSetMetaData implements ResultSetMetaData{
 
     @Override
     public int getColumnType(int columnIndex) throws SQLException {
-        return table.getColumn(adjustColumnIndex(columnIndex)).getDataType().ordinal();
+        Entity.DATA_TYPE x = table.getColumn(adjustColumnIndex(columnIndex)).getDataType();
+        switch (x){
+            case DT_BOOL: return Types.BOOLEAN;
+            case DT_BYTE: return Types.CHAR;
+            case DT_SHORT: return Types.TINYINT;
+            case DT_INT: return Types.INTEGER;
+            case DT_LONG: return Types.BIGINT;
+            case DT_DATE: return Types.DATE;
+            case DT_TIME: return Types.TIME;
+            case DT_DATETIME:
+            case DT_TIMESTAMP: return Types.TIMESTAMP;
+            case DT_FLOAT: return Types.FLOAT;
+            case DT_DOUBLE: return Types.DOUBLE;
+            case DT_STRING: return Types.VARCHAR;
+            case DT_BLOB: return Types.VARCHAR;
+            default: return Types.VARCHAR;
+        }
+
     }
 
     @Override
