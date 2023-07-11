7a087131ef986c0ebbda49edd4b7fb44727410e4
chengyitian <sylvestercheng@aliyun.com>
2023-07-11 10:46:45 +0800

    AJ-362: rewrite JDBCResultSetMetaData getColumnType(int columnIndex).
    Former-commit-id: 7fa82704cd686ddaea9e1721ed114083a5c25db9

--- src/com/dolphindb/jdbc/JDBCResultSetMetaData.java
+++ src/com/dolphindb/jdbc/JDBCResultSetMetaData.java
@@ -8,7 +8,7 @@ import java.sql.Types;
 
 public class JDBCResultSetMetaData implements ResultSetMetaData{
 
-    private BasicTable table;
+    private final BasicTable table;
 
     public JDBCResultSetMetaData(BasicTable table){
         this.table = table;
@@ -46,8 +46,40 @@ public class JDBCResultSetMetaData implements ResultSetMetaData{
 
 
     @Override
+//    public int getColumnType(int columnIndex) throws SQLException {
+//        return table.getColumn(adjustColumnIndex(columnIndex)).getDataType().ordinal();
+//    }
     public int getColumnType(int columnIndex) throws SQLException {
-        return table.getColumn(adjustColumnIndex(columnIndex)).getDataType().ordinal();
+        Entity.DATA_TYPE x = table.getColumn(adjustColumnIndex(columnIndex)).getDataType();
+        switch (x){
+            case DT_BOOL:
+                return Types.BOOLEAN;
+            case DT_BYTE:
+                return Types.CHAR;
+            case DT_SHORT:
+                return Types.TINYINT;
+            case DT_INT:
+                return Types.INTEGER;
+            case DT_LONG:
+                return Types.BIGINT;
+            case DT_DATE:
+                return Types.DATE;
+            case DT_TIME:
+                return Types.TIME;
+            case DT_DATETIME:
+            case DT_TIMESTAMP:
+                return Types.TIMESTAMP;
+            case DT_FLOAT:
+                return Types.FLOAT;
+            case DT_DOUBLE:
+                return Types.DOUBLE;
+            case DT_STRING:
+                return Types.VARCHAR;
+            case DT_BLOB:
+                return Types.VARCHAR;
+            default:
+                return Types.VARCHAR;
+        }
     }
 
     @Override
