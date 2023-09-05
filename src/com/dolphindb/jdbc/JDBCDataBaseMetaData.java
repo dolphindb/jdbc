769afd24690ba421dd2cf6eaaa6578bdf05f7e8a
Yuhang Lu <yuhang.lu@dolphindb.com>
2023-09-05 11:55:23 +0800

    JAVAOS-156,JAVAOS-159 : fix bug

--- src/com/dolphindb/jdbc/JDBCDataBaseMetaData.java
+++ src/com/dolphindb/jdbc/JDBCDataBaseMetaData.java
@@ -173,7 +173,7 @@ public class JDBCDataBaseMetaData implements DatabaseMetaData {
 
     @Override
     public String getIdentifierQuoteString() {
-        return "`";
+        return "";
     }
 
     @Override
